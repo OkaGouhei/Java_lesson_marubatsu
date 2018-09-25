defec7fcaff92d906f6e03cee7381b045998d4d1
OkaGouhei <okagouhei@okagoutairanoMacBook-puro.local>
2018-09-25 10:14:22 +0900

    java4
    コンピューターと対戦できた。
    盤面、まるばつの表示をメソッドにした。
    盤面のサイズを自由に変えれるようにした。
    コンピューターがどこに置くかのメソッドは作れなかった。

--- marubatsu.java
+++ marubatsu.java
@@ -1,16 +1,26 @@
 import java.util.Scanner;
+import java.util.Random;
 public class marubatsu{
+  public static void Printcell(String[][]cell,int SideNum){
+    for(int i=0;i<SideNum;i++){
+      for(int j=0; j<SideNum;j++)
+      {
+      System.out.print("|"+(cell[i][j]));
+      }
+      System.out.println("|");
+    }
+  }
   public static void main(String[] args){
-    String[][]cell= new String[3][3];
-    for(int i=0;i<=2;i++){
-      for(int j=0; j<=2;j++){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("盤面の列の数を指定してください。");
+    int SideNum = scanner.nextInt();
+    String[][]cell= new String[SideNum][SideNum];
+    for(int i=0;i<SideNum;i++){
+      for(int j=0; j<SideNum;j++){
         cell[i][j] = " ";
       }
     }
-    Scanner scanner = new Scanner(System.in);
-    int numA;
-    int numB;
-    for(int m=1;m<=9;m++){
+    for(int m=1;m<= SideNum*SideNum ;m++){
       String player;
       if(m % 2 == 1){
         player = "○";
@@ -22,36 +32,65 @@ public class marubatsu{
       System.out.println("数値を2つ入力してください。");
       boolean is_set = false;
       while(!is_set){
-        numA = scanner.nextInt();
-        numB = scanner.nextInt();
-        if (cell[numA][numB] != " "){
-        System.out.println("すでに入力済みです。");
+        int numA;
+        int numB;
+        if(m % 2 == 1){
+          numA = scanner.nextInt();
+          numB = scanner.nextInt();
+        }
+        else{
+          Random rnd = new Random();
+          numA = rnd.nextInt(SideNum);
+          numB = rnd.nextInt(SideNum);
+        }
+        if(numA<0 ||numA>= SideNum||numB<0 ||numB >= SideNum){
+          System.out.println("範囲外です");
+        }
+        else if (cell[numA][numB] != " "){
+          System.out.println("すでに入力済みです。");
         }
         else{
         cell[numA][numB] = player ;
         is_set = true;
         }
       }
-      for(int i=0;i<=2;i++){
-        System.out.println((cell[i][0])+"|"+(cell[i][1])+"|"+(cell[i][2]));
-      }
-      if((cell[0][0]==player)&&(cell[1][1]==player)&&(cell[2][2]==player)){
-        System.out.println(player+"の勝ちです。");
-        return;
-      }
-      if((cell[2][0]==player)&&(cell[1][1]==player)&&(cell[0][2]==player)){
-        System.out.println(player+"の勝ちです。");
-        return;
-      }
-      for(int i=0;i<=2;i++){
-        if((cell[i][0]==player)&&(cell[i][1]==player)&&(cell[i][2]==player)){
-        System.out.println(player+"の勝ちです。");
-        return;
+      Printcell(cell,SideNum);
+      //ここから勝敗の判定
+      row: for(int i=0; i<SideNum; i++){
+        for(int j=0; j<SideNum; j++){
+         if(cell[i][j] != player)continue row;
         }
-        else if((cell[0][i]==player)&&(cell[1][i]==player)&&(cell[2][i]==player)){
         System.out.println(player+"の勝ちです。");
         return;
+      }
+      row: for(int j=0; j<SideNum; j++){
+        for(int i=0; i<SideNum; i++){
+         if(cell[i][j] != player)continue row;
         }
+        System.out.println(player+"の勝ちです。");
+        return;
+      }
+      //右下斜めの判定
+      is_set = false;
+      for(int i=0;i<SideNum; i++){
+        if(cell[i][i] != player){
+          is_set = true;
+        }
+      }
+      if(is_set == false){
+        System.out.println(player+"の勝ちです。");
+        return;
+      }
+      //右上斜めの判定
+      is_set = false;
+      for(int i=0;i<SideNum; i++){
+        if(cell[i][SideNum-1-i] != player){
+          is_set = true;
+        }
+      }
+      if(is_set == false){
+        System.out.println(player+"の勝ちです。");
+        return;
       }
     }
     System.out.println("あいこです。");
